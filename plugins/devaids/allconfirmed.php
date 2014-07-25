208cf9eb74bf4aec83de537def12e974677c774f
Michiel Dethmers <git@phplist.com>
2014-07-25 12:39:25 +0100

    unblacklist all as well

--- plugins/devaids/allconfirmed.php
+++ plugins/devaids/allconfirmed.php
@@ -1,4 +1,8 @@
 <?php
 
 Sql_Query(sprintf('update %s set confirmed = 1',$GLOBALS['tables']['user']));
+Sql_Query(sprintf('update %s set blacklisted = 0',$GLOBALS['tables']['user']));
+Sql_Query(sprintf('delete from %s',$GLOBALS['tables']['user_blacklist']));
+
+print s('Marked all subscribers confirmed and not blacklisted, and cleared the blacklist');
 
